b750132d308962cc060f3eae75df2697e0d7e08b
miftahudin19 <40747706+miftahudin19@users.noreply.github.com>
2018-12-17 21:20:12 +0700

    Add files via upload

---
+++ Program Konversi Dollar ke Rupiah.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int Rp,Dollar;
+	cout <<"Masukan Nilai Dolar yang ingin di konvers ke Rupiah : ";
+	cin >>Dollar;
+	
+	Rp = 15149 * Dollar;
+	cout <<"Hasil Kovers Dollar ke Rupiah adalah = " <<Rp;
+	
+	return 0;
+}
